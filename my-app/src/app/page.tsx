d8eb8d81a7d6c08c55eeca18cd880a34b2e9c6fb
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2025-04-10 22:12:50 -0300

    Estrutura inicial do projeto

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-500 w-screen h-screen flex flex-auto justify-center items-center font-sans">
+      <h1 className="text-3xl text-white">
+        Home
+      </h1>
+    </div>
+  )
+}
