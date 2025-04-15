e3864d7286a3082da21882a9b57f175b83ddcd7f
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2025-04-14 21:50:34 -0300

    Alteracoes no conteudo

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-[100%] h-[70vh] flex flex-auto justify-center font-sans">
-      <div className=" flex m-auto bg-pink-400 w-80 h-80 rounded-2xl justify-center content-center">
+      <div className=" flex m-auto bg-linear-to-tl from-fuchsia-500 to-violet-500 w-80 h-80 rounded-2xl justify-center content-center">
         
         <h1 className="text-xl text-black m-auto">
           Não perca seu tempo e compre já o seu!
