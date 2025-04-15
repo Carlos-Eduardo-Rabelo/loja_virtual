66262ffbdf31bf1c63f7a32bf948f27820ecc961
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2025-04-14 21:21:41 -0300

    Primeira frase

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <div className="w-[100%] h-[70vh] flex flex-auto justify-center font-sans">
       <div className=" flex m-auto bg-pink-400 w-80 h-80 rounded-2xl justify-center content-center">
-        <h1 className="text-3xl text-black m-auto">
-          Home
+        
+        <h1 className="text-xl text-black m-auto">
+          Não perca seu tempo e compre já o seu!
         </h1>
       </div>
       
